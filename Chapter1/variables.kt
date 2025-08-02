02f1fcf77d91afbaf42f1106421e0aced6f4c862
William Doran <wfdoran4@yahoo.com>
2025-08-02 15:58:09 -0700

    Basic variable types.

---
+++ Chapter1/variables.kt
@@ -0,0 +1,23 @@
+fun main(args : Array<String>) {
+    val name = "John Doe"
+    val x = 42
+    val y = 42L
+    val z: Long = x.toLong()
+
+    println(name)
+    println(x)
+    println(y)
+    println(z)
+
+    val q = 44.5
+    val qq = q.toDouble()
+    val qqq = q.toInt()
+
+    println(q)
+    println(qq)
+    println(qqq)
+
+    val s = "Hello All"
+    println(s)
+
+}
